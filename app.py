0426f8f78d614134d3d95cc730f01c805991eaa2
Garrepalli Dhanush <35004425+Dhanush-Garrepalli@users.noreply.github.com>
2024-02-24 18:50:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ import streamlit as st
 import pandas as pd
 
 # Load the dataset
-url = 'https://raw.githubusercontent.com/Dhanush-Garrepalli/AgriAssist/main/dataset_soil_nutrients.csv'
+url = 'https://raw.githubusercontent.com/Dhanush-Garrepalli/AgriAssist/main/dataset_soil_nutrients_7.csv'
 df = pd.read_csv(url)
 
 # Prepare the data
-X = df[['N', 'P', 'K', 'pH', 'EC', 'OC', 'S', 'Zn', 'Fe', 'Cu', 'Mn', 'B']]  # Features
+X = df[['N', 'P', 'K', 'pH','Zn', 'Fe']]
 y = df['Output']  # Target variable
 
 # Split the data
@@ -39,22 +39,14 @@ ph = st.number_input('Enter pH level:')
 n = st.number_input('Enter Nitrogen level:')
 p = st.number_input('Enter Phosphorus level:')
 k = st.number_input('Enter Potassium level:')
-
-ec = st.number_input('Enter EC level:', key='ec')
-oc = st.number_input('Enter OC level:', key='oc')
-s = st.number_input('Enter S level:', key='s')
 zn = st.number_input('Enter Zn level:', key='zn')
 fe = st.number_input('Enter Fe level:', key='fe')
-cu = st.number_input('Enter Cu level:', key='cu')
-mn = st.number_input('Enter Mn level:', key='mn')
-b = st.number_input('Enter B level:', key='b')
-
 
 # Add inputs for the rest of the features
 
 if st.button('Analyze'):
     # Note: Add the rest of your feature inputs here in the correct order
-    input_features = scaler.transform([[n, p, k, ph, ec,oc,s,zn,fe,cu,mn,b]])  # Fill in the rest of the inputs
+    input_features = scaler.transform([[n, p, k, ph,zn,fe]])  # Fill in the rest of the inputs
     prediction = model.predict(input_features)
     if prediction[0] == 0:
         result = "Low Fertile"
