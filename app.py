d355a6224bec08112b75a67fb5e7ef7cb28259a8
Garrepalli Dhanush <35004425+Dhanush-Garrepalli@users.noreply.github.com>
2024-02-25 13:35:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ if st.button('Analyze'):
     user_input = user_input_array.reshape(1, -1)  # Reshape to match expected input shape
     input_scaled = scaler.transform(user_input)  # Scale the user input
     input_pca = pca.transform(input_scaled)  # Transform user input with PCA
-    
+
     # Make prediction
     prediction = model.predict(input_pca)
     result = "High Fertile" if prediction[0] == 1 else "Low Fertile"
@@ -60,30 +60,113 @@ if st.button('Analyze'):
 
     # Provide feedback based on mean thresholds for the top features only
     for feature in top_features:
-        if input_values[feature] < mean_thresholds[feature]:
-            feedback_message = f'{feature} level is below average, which may affect crop yield.'
-            if feature.startswith('N'):
-                feedback_message += 'Nitrogen-deficient plants produce smaller than normal fruit, leaves, and shoots and these can develop later than normal.'
-            if feature.startswith('P'):
-                feedback_message += 'Phosphorus deficiency can cause leaves to darken and take on a dull, blue-green hue, which may lighten to pale in more extreme cases.'
-            if feature.startswith('K'):
-                feedback_message += 'Potassium deficiency in broadleaves causes leaves to turn yellow and then brown at the tips and margins and between veins.'
-            if feature.startswith('p'):
-                feedback_message += 'Low PH levels makes plant growth slower'
-            if feature.startswith('Z'):
-                feedback_message += 'Zinc deficiency negatively affects plant growth.'
-            if feature.startswith('Fe'):
-                feedback_message += 'Iron deficiency will turn leaves to yellow'
-            if feature.startswith('E'):
-                feedback_message += 'Low EC level'
-            if feature.startswith('O'):
-                feedback_message += 'Low OC level'
-            if feature.startswith('S'):
-                feedback_message += 'Sulphur deficiency will make yellow coloration of the older leaves'
-            if feature.startswith('C'):
-                feedback_message += ' Deficiency of copper can lead to increased susceptibility to diseases like ergot, which can cause significant yield loss in small grains.'
-            if feature.startswith('M'):
-                feedback_message += 'Manganese deficiency in new leaves to be yellow to whitish with relatively wide, green areas along the veins.'
-            if feature.startswith('B'):
-                feedback_message += 'Boron deficiency will cause crinkling of leaves'
-            st.write(feedback_message)
+        feature_value = input_values[feature]
+        feedback_message = f'{feature} level is {feature_value}. '
+        if feature.startswith('N'):
+            feedback_message = f'Nitrogen level is {feature_value}. '
+            if feature_value < 280:
+                feedback_message += "Nitrogen is deficient, which may leads to stunted growth and yellowing of leaves."
+            elif feature_value > 560:
+                feedback_message += "Nitrogen level is high,can result in excessive foliage growth at the expense of fruit and flower development, and can also cause nitrogen burn."
+            else:
+                feedback_message += "The Nitrogen level is optimal for the crop, promoting improved yield."
+        if feature.startswith('P'):
+            feedback_message = f'Phosphorous level is {feature_value}. '
+            if feature_value < 22.5:
+                feedback_message += "Phosphorus is deficient, results in stunted growth and dark, dull, or purple-tinged leaves."
+            elif feature_value > 55:
+                feedback_message += "Phosphorus level is high, rare, but can inhibit the uptake of other nutrients like zinc and iron."
+            else:
+                feedback_message += "The Phosphorus level is optimal for the crop, promoting improved yield."
+
+        if feature.startswith('K'):
+            feedback_message = f'Potassium level is {feature_value}. '
+            if feature_value < 140:
+                feedback_message += "Potassium is deficient, causes yellowing at leaf edges, weak stems, and reduced growth."
+            elif feature_value > 330:
+                feedback_message += "Potassium level is high, can lead to magnesium deficiency and salt stress in plants."
+            else:
+                feedback_message += "The Potassium level is optimal for the crop, promoting improved yield."
+
+        if feature.startswith('p'):
+            feedback_message = f'ph level is {feature_value}. '
+            if feature_value < 5.5:
+                feedback_message += "ph is deficient, Nutrient solubility can increase to toxic levels; aluminum and manganese may become toxic."
+            elif feature_value > 7.5:
+                feedback_message += "ph level is high, Reduced solubility of micronutrients like iron, leading to deficiencies."
+            else:
+                feedback_message += "The ph level is optimal for the crop, promoting improved yield."
+
+        if feature.startswith('Z'):
+            feedback_message = f'Zinc level is {feature_value}. '
+            if feature_value < 0.6:
+                feedback_message += "Zinc is deficient, Leads to stunted growth, smaller leaves, and interveinal chlorosis."
+            elif feature_value > 1.5:
+                feedback_message += "Zinc level is high, Can inhibit plant growth and reduce iron absorption"
+            else:
+                feedback_message += "The Zinc level is optimal for the crop, promoting improved yield."
+
+        if feature.startswith('Fe'):
+            feedback_message = f'Fe level is {feature_value}. '
+            if feature_value < 0.6:
+                feedback_message += "Iron is deficient, Causes chlorosis in young leaves, reduced flowering and fruiting."
+            elif feature_value > 1.5:
+                feedback_message += "Iron level is high,Rare in plants, but can cause bronzing and chlorosis."
+            else:
+                feedback_message += "The Iron level is optimal for the crop, promoting improved yield."
+
+        if feature.startswith('E'):
+            feedback_message = f'EC level is {feature_value}. '
+            if feature_value < 200:
+                feedback_message += "EC level is deficient, Indicates insufficient nutrients in the soil."
+            elif feature_value > 1600:
+                feedback_message += "EC level is high,Can lead to nutrient toxicity and osmotic stress, affecting plant water uptake."
+            else:
+                feedback_message += "The EC level is optimal for the crop, promoting improved yield."
+
+        if feature.startswith('O'):
+            feedback_message = f'OC level is {feature_value}. '
+            if feature_value < 0.4:
+                feedback_message += "OC level is deficient,results in poor soil structure and reduced water retention."
+            elif feature_value > 3:
+                feedback_message += "OC level is high,Generally beneficial but can lead to imbalances if not matched with nutrient availability."
+            else:
+                feedback_message += "The OC level is optimal for the crop, promoting improved yield."
+
+        if feature.startswith('S'):
+            feedback_message = f'Sulphur level is {feature_value}. '
+            if feature_value < 10:
+                feedback_message += "Sulphur level is deficient,Causes yellowing of leaves, similar to nitrogen deficiency."
+            elif feature_value > 20:
+                feedback_message += "Sulphur level is high,Rare, but can lead to decreased growth and enzyme inhibition."
+            else:
+                feedback_message += "The Sulphur level is optimal for the crop, promoting improved yield."
+
+        if feature.startswith('C'):
+            feedback_message = f'Copper level is {feature_value}. '
+            if feature_value < 0.2:
+                feedback_message += "Copper level is deficient,Leads to wilting, repressed growth, and chlorosis."
+            elif feature_value > 5:
+                feedback_message += "Copper level is high, can cause toxicity symptoms like leaf chlorosis and stunted growth."
+            else:
+                feedback_message += "The Copper level is optimal for the crop, promoting improved yield."
+
+        if feature.startswith('M'):
+            feedback_message = f'Manganese level is {feature_value}. '
+            if feature_value < 2:
+                feedback_message += "Manganese level is deficient, results in interveinal chlorosis, necrotic spots, and reduced growth."
+            elif feature_value > 4:
+                feedback_message += "Manganese level is high, can lead to iron deficiency symptoms and reduced growth."
+            else:
+                feedback_message += "The Manganese level is optimal for the crop, promoting improved yield."
+
+        if feature.startswith('B'):
+            feedback_message = f'Boron level is {feature_value}. '
+            if feature_value < 2:
+                feedback_message += "Boron level is deficient, causes death of meristem tissue, brittle leaves, and stunted growth."
+            elif feature_value > 4:
+                feedback_message += "Boron level is high, Leads to leaf burn, chlorosis, and necrosis."
+            else:
+                feedback_message += "The Boron level is optimal for the crop, promoting improved yield."
+
+        st.write(feedback_message)
